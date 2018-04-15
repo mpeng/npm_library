f26fc9f2cf95f23891188d991c254907431ce2a9
mpeng <mpeng@bridge2solutions.com>
2018-04-14 22:19:44 -0400

    initial checkin

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+  console.log("This is a message from the demo package 5");
 }
+
+exports.prettify = function ( object ) {
+		return JSON.parse(JSON.stringify(object));
+}
+
+exports.printify = function ( object ) {
+		console.dir(prettify(object), {depth: null, colors: true});
+}
+
+/*
+exports.printMsg = function() {
+  console.log("This is a message from the demo package !");
+}
+*/
+
+
+/*
+module.exports = {
+	prettify: function ( object ) {
+		return JSON.parse(JSON.stringify(object));
+	},
+
+	printify: function ( object ) {
+		console.dir(prettify(object), {depth: null, colors: true})
+	}
+
+	printMsg:  function() {
+  		console.log("This is a message from the demo package !");
+	}		  
+}
+*/
+
 
