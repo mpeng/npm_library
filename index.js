32c45d090fb28a9a3a39a6e823f85832100b9ee2
mpeng <mpeng@bridge2solutions.com>
2018-04-16 09:16:13 -0400

    all comments

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 exports.printMsg = function() {
-  console.log("This is a message from the demo package 5");
+  console.log("This is a message from the demo package version #9");
 }
 
 exports.prettify = function ( object ) {
 		return JSON.parse(JSON.stringify(object));
 }
 
+function prettify( object ) {
+	return JSON.parse(JSON.stringify(object));
+}
+
 exports.printify = function ( object ) {
 		console.dir(prettify(object), {depth: null, colors: true});
 }
+
+
 
 /*
 exports.printMsg = function() {
